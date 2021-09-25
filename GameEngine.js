700c61f91a08aa99068b339a54ef79da1977759e
DashCampbell <90148639+DashCampbell@users.noreply.github.com>
2021-09-25 09:08:47 -0600

    Made Changes

--- GameEngine.js
+++ GameEngine.js
@@ -1,3 +1,14 @@
+/**
+ * 
+I tried making a javascript game engine that operates similarly to the olcPixelGameEngine created by Javidx9 at 
+https://github.com/OneLoneCoder/olcPixelGameEngine.git
+
+Basic Features Include:
+    A game loop function.
+    Handling keyboard input(key is pressed/held/or released)
+    Render functions.
+ */
+
 // <--------------EXAMPLE IMPLEMENTAION---------------------->
 //game.js
 /*
